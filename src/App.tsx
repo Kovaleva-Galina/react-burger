36e4c0819c31a8785393d6175f9afce5af983bd2
Galina Kovaleva <galkina.galina.alexandrovna@yandex.ru>
2023-03-17 13:05:05 +0300

    Firs-comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 
 function App() {
+  const [current, setCurrent] = React.useState('one');
   return (
     <div className="App">
+       <Tab value="one" active={current === 'one'} onClick={setCurrent}>
+        One
+      </Tab>
+      <Tab value="two" active={current === 'two'} onClick={setCurrent}>
+        Two
+      </Tab>
+      <Tab value="three" active={current === 'three'} onClick={setCurrent}>
+        Three
+      </Tab>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
