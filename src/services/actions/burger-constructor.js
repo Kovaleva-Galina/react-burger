8b0a4ce076e2890fb913abbf0e175d34b8d0ae35
Galina Kovaleva <galkina.galina.alexandrovna@yandex.ru>
2023-05-10 12:42:14 +0300

    fix: исправление функции удаления ингредиента

--- src/services/actions/burger-constructor.js
+++ src/services/actions/burger-constructor.js
@@ -4,7 +4,7 @@ export const UPDATE_SELECTED_FILLINGS = 'UPDATE_SELECTED_FILLINGS';
 export const UPDATE_SELECTED_BUNS = 'UPDATE_SELECTED_BUNS';
 export const ADD_SELECTED_FILLING = 'ADD_SELECTED_FILLING';
 
-export const deleteSelectedFilling = (id) => ({type:DELETE_SELECTED_FILLING, id});
+export const deleteSelectedFilling = (payload) => ({type:DELETE_SELECTED_FILLING, payload});
 export const updateSelectedFillings =(payload) => ({type: UPDATE_SELECTED_FILLINGS, payload});
 export const updateSelectedBuns = (payload) => ({type: UPDATE_SELECTED_BUNS, payload});
 export const addSelectedFilling =(payload) => ({type: ADD_SELECTED_FILLING, payload})
